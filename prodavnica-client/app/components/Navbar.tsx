10e6328d37a6c5b39f40c061511254fe9c46030b
kosovic-creator <drasko.kosovic@icloud.com>
2026-02-27 18:33:42 +0100

    navbar

--- prodavnica-client/app/components/Navbar.tsx
+++ prodavnica-client/app/components/Navbar.tsx
@@ -60,11 +60,16 @@ const Navbar: React.FC<NavbarProps> = ({ setSidebarOpen }) => {
               >
                 <FaBars className="w-6 h-6 sm:w-7 sm:h-7 text-white" />
               </Button>
-               <Link href="/" className="text-xl font-bold">
-                <svg className="w-8 h-8 text-amber-500" fill="currentColor" viewBox="0 0 20 20">
-                  <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
-                </svg>
-        </Link>
+              <Button variant="ghost"
+                className="px-2 py-2 sm:px-3 focus:outline-none rounded-lg hover:bg-gray-700 transition-colors"
+                asChild
+              >
+                <Link href="/">
+                  <svg className="w-6 h-6 sm:w-7 sm:h-7 text-amber-100" fill="currentColor" viewBox="0 0 20 20">
+                    <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
+                  </svg>
+                </Link>
+              </Button>
             </div>
             {/* Center Section - Desktop Search */}
             <div className="hidden lg:flex w-full max-w-md mx-2 sm:mx-4">
